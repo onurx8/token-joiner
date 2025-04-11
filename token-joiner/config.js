2ef2765e8b8d1d42fc7721789618f3da613c9e83
Onur <92668572+onurx8@users.noreply.github.com>
2025-04-11 23:37:24 +0300

    onurx
    onurx

---
+++ token-joiner/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    data: {
+        guildId: "",
+    },
+    bot: {
+        id: "",
+        token: "",
+        secret: "",
+        status: "idle",
+        statusName: "Onurx - Üye",
+    },
+    web: {
+        url: "http://localhost:3001",
+        port: 3001,
+    }
+}
